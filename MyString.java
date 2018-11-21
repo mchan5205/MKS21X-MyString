53cca5da12f0b80e24e1826b4f1fb2d2cf8257f3
Michael Chan <mchan10@stuy.edu>
2018-11-21 10:43:40 -0500

    added many methods

--- MyString.java
+++ MyString.java
@@ -6,4 +6,24 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
       data[i] = s.subSequence(i, i + 1);
     }
   }
+  public char charAt(int index){
+    return data[index];
+  }
+  public int length(){
+    return data.length;
+  }
+  public CharSequence subSequence(int start, int end){
+    String y = "";
+    for (int i = start; i < end; i ++){
+      y += data[i];
+    }
+    MyString x = new MyString(y);
+    return x;
+  }
+  public String toString(){
+    String y = "";
+    for (int i = 0; i < data.length; i ++){
+      y += data[i];
+    }
+  }
 }
